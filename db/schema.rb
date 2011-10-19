6e00acc4f8cebada8bb4ffd4f3c503ac06ac0a76
Fredrik Bergljung <fredrik.bergljung@valtech.se>
2011-10-19 12:08:13 +0200

    added services scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111019093115) do
+ActiveRecord::Schema.define(:version => 20111019100721) do
 
   create_table "services", :force => true do |t|
     t.string   "street"
